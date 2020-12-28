af5b4d8fc0c42ec5f3f4e07d00e3a2714a99e27f
Rituparna Warwatkar <rwarwatkar@gmail.com>
2020-12-28 15:00:51 +0530

    16

---
+++ assignment2/16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter a number\n");
+    scanf("%d", &num);
+    int d1 = num % 10;
+    num /= 10;
+    int d2 = num % 10;
+    num /= 10;
+    int d3 = num % 10;
+    num /= 10;
+    int d4 = num % 10;
+    num /= 10;
+    int d5 = num % 10;
+    num /= 10;
+    printf("%d %d %d %d %d", d1, d2, d3, d4, d5);
+    int sum = d1 + d2 + d3 + d4 + d5;
+    printf("%d", sum);
+
+    return 0;
+}
