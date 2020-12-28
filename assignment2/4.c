316f3990e3c60e855658e5632bb98cefcd50c3a2
Rituparna Warwatkar <rwarwatkar@gmail.com>
2020-12-28 13:12:59 +0530

    4

---
+++ assignment2/4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float l, w;
+    printf("Enter lenght");
+    scanf("%f", &l);
+    printf("Enter width", &w);
+    scanf("%f", &w);
+    printf("The area of rectangle is %.2f", l * b);
+    return 0;
+}
