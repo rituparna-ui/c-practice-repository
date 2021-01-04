f62ffbde1b36cc6be600c73fb06b7bb62ba4257a
Rituparna Warwatkar <rwarwatkar@gmail.com>
2021-01-04 13:22:49 +0530

    6

---
+++ assignment3/6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5, b = 7;
+    if (a > b)
+    {
+        printf("a is greater");
+    }
+    else if (a < b)
+    {
+        printf("b is greater");
+    }
+    else
+    {
+        printf("both equal");
+    }
+
+    return 0;
+}
