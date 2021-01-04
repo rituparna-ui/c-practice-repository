ea42f28b044e7df505b201ae744def0b1a0dd1b3
Rituparna Warwatkar <rwarwatkar@gmail.com>
2021-01-04 13:12:47 +0530

    2

---
+++ assignment3/2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num = 7;
+    if (num % 2)
+    {
+        printf("Numer is odd");
+    }
+    else
+    {
+        printf("Numer is odd");
+    }
+
+    return 0;
+}
