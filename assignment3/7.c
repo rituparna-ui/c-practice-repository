14f8e02c072447826550f2a8516662147127b691
Rituparna Warwatkar <rwarwatkar@gmail.com>
2021-01-04 13:26:08 +0530

    7

---
+++ assignment3/7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    // 65-122
+    char a = 'R';
+    if ((a >= 65) || (a <= 122))
+    {
+        printf("It is a englich character");
+    }
+    else
+    {
+        printf("it is not a english character");
+    }
+
+    return 0;
+}
